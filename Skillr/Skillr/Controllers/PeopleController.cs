69ca0c7a0a2b30eda478cdbf3c88f07d341f0554
Natalie <nataliefruitema@gmail.com>
2018-11-05 19:48:54 +0100

    Added more validations and added searchbar

--- Skillr/Skillr/Controllers/PeopleController.cs
+++ Skillr/Skillr/Controllers/PeopleController.cs
@@ -23,7 +23,6 @@ namespace Skillr.Controllers
         {
             return View(await _context.Person.ToListAsync());
         }
-
         // GET: People/Details/5
         public async Task<IActionResult> Details(int? id)
         {
