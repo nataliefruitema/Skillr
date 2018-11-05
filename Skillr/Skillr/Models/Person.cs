6c6da22f70f452cffa5a049a46552836c9e6f375
Wesley <wesley.bekkers@gmail.com>
2018-11-05 23:01:27 +0100

    Add Person and project dropdown to Manager. ToDo is Skills list

--- Skillr/Skillr/Models/Person.cs
+++ Skillr/Skillr/Models/Person.cs
@@ -27,7 +27,7 @@ namespace Skillr.Models
         // If person not available
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] 
-        public DateTime OnProjectUntil { get; set; }
+        public DateTime? OnProjectUntil { get; set; }
 
     }
 }
